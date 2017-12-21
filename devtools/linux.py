11bf490a2944c536dce488a0d79f753ef4ddbb81
AIJIJI <dand@mail.ustc.edu.cn>
2017-12-21 19:42:02 +0800

    :sparkles:lxrun(daemon=True) returns a Process Class object now.\n(Which means you can do something easily like get the pid child process.

--- devtools/linux.py
+++ devtools/linux.py
@@ -7,14 +7,14 @@ __all__ = ['lxrun']
 
 def lxrun(cmd, err=False, daemon=False):
     if daemon is True:
-        Popen(cmd, shell=True, stdout=open(os.devnull, 'w'))
-        return
+        p = Popen(cmd, shell=True, stdout=open(os.devnull, 'w'))
+        return p 
         
     res = None
     p =  Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
     stdout, stderr = p.communicate()
-    stdout = tostr(stdout)
-    stderr = tostr(stderr)
+    stdout = tostr(stdout).rstrip()
+    stderr = tostr(stderr).rstrip()
     if err:
         return stdout, stderr
     else:
