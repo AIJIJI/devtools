a382f5a6f59d5809b4f43a41fb1f84960fc1edca
AIJIJI <dand@mail.ustc.edu.cn>
2017-12-25 13:26:37 +0800

    :bookmark:Version 0.2.1 release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='AIJIdevtools',
-    version='0.1.0',
+    version='0.2.1',
     author='AIJI',
     author_email='thecrazyaiji@gmail.com',
     description='Some useful helper-funcs for devpers',
