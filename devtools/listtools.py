79b8a23ab434de87bc66c7dd82ccc76b587e5db8
AIJIJI <dand@mail.ustc.edu.cn>
2018-04-19 05:31:16 -0400

    needs more cow bell

--- devtools/listtools.py
+++ devtools/listtools.py
@@ -10,6 +10,9 @@ def partition(ls, size):
 
 
 if __name__ == '__main__':
+    from math import ceil
     ls = [1, 2, 3, 4, 5]
     foo = partition(ls, 2)
+    assert foo == [[1, 2], [3, 4], [5]]
+    foo = partition(ls, ceil(len(ls)/3))
     assert foo == [[1, 2], [3, 4], [5]]
