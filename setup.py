6739fb8508681d9b2f82a0838c69b63a2d58fddc
AIJIJI <dand@mail.ustc.edu.cn>
2018-05-10 15:46:33 +0800

    I had a cup of tea and now it's fixed

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup
 
 setup(
     name='AIJIdevtools',
-    version='0.3.2',
+    version='0.3.3',
     author='AIJI',
     author_email='thecrazyaiji@gmail.com',
     description='Some useful helper-funcs for devpers',
     packages=['devtools'],
-    install_requires=['psutil'],
+    install_requires=['psutil', 'sqlparse'],
     url='https://github.com/AIJIJI/devtools'
                                                                                 )
