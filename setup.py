7bb2cb03b55c65c9e0d6edaf97f18e759cc5439d
AIJIJI <dand@mail.ustc.edu.cn>
2018-08-08 18:30:34 +0800

    New release 0.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='AIJIdevtools',
-    version='0.5.0',
+    version='0.5.1',
     author='AIJI',
     author_email='thecrazyaiji@gmail.com',
     description='Some useful helper-funcs for devpers',
@@ -11,7 +11,14 @@ setup(
     install_requires=[
         'sh',
         'sqlparse',
-        'termcolor',
+        'termcolor'
     ],
-    url='https://github.com/AIJIJI/devtools'
+    url='https://github.com/AIJIJI/devtools',
+    classifiers=(
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Framework :: Flask",
+        "Development Status :: 1 - Planning",
+    )
 )
