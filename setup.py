605dde9f5a8de19fbc5b9bf5c7213928bbf48842
AIJIJI <dand@mail.ustc.edu.cn>
2018-08-14 15:32:45 +0800

    I would rather be playing SC2.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='AIJIdevtools',
-    version='0.5.1',
+    version='0.5.2',
     author='AIJI',
     author_email='thecrazyaiji@gmail.com',
     description='Some useful helper-funcs for devpers',
