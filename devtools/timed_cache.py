fdba020a08361b5f97892cd0f062bf39222cb9af
AIJIJI <dand@mail.ustc.edu.cn>
2018-08-14 16:10:37 +0800

    Don't Ask Me, I Have No Idea Why This Works Either

--- devtools/timed_cache.py
+++ devtools/timed_cache.py
@@ -1,14 +1,17 @@
+'''
+    类似于 lru_cache, 但提供限时的缓存
+'''
+
 from datetime import datetime, timedelta
 import functools
 
 
-def timed_cache(**timedelta_kwargs):
-
+def timed_cache(maxsize=128, typed=False, **timedelta_kwargs):
     def _wrapper(f):
         update_delta = timedelta(**timedelta_kwargs)
         next_update = datetime.utcnow() - update_delta
         # Apply @lru_cache to f with no cache size limit
-        f = functools.lru_cache(None)(f)
+        f = functools.lru_cache(maxsize, typed)(f)
 
         @functools.wraps(f)
         def _wrapped(*args, **kwargs):
